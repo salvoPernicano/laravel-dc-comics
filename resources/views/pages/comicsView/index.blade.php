1f9bb56fb08df5b05225b5214817a86503c0438d
salvoPernicano <salvo.pernicano@gmail.com>
2024-03-19 15:55:15 +0100

    aggiunta sass

--- resources/views/pages/comicsView/index.blade.php
+++ resources/views/pages/comicsView/index.blade.php
@@ -5,7 +5,7 @@
 
 @section('main')
 <main>
-    <h2>Index di Comics</h2>
+    <h2 class="pageHeader">Lista Fumetti</h2>
     <div class="container">
         @foreach($comics as $element)
         <div class="card">
@@ -13,14 +13,16 @@
             <a href="{{route('comics.show', ['comic' => $element['id']])}}">
                 <h2>{{$element['title']}}</h2>
             </a>
-            <p>{{$element['description']}}</p>
-            <h3>{{$element['series']}}</h3>
-            <h3>{{$element['price']}}</h3>
+            <div class="textContainer">
+                <h5>Description: </h5>
+                <p>{{$element['description']}}</p>
+
+            </div>
+            <h3>Serie: {{$element['series']}}</h3>
+            <h3>${{$element['price']}}</h3>
         </div>
         @endforeach
-        <div>
-            <a class="button" role="button" href="{{route('comics.create')}}">Crea</a>
-        </div>
     </div>
+        <a class="button" role="button" href="{{route('comics.create')}}">Inserisci nuovo</a>
 </main>
 @endsection
