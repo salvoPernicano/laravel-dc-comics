49c782edb68342fc0c4be3c6c689ea60061d2d41
salvoPernicano <salvo.pernicano@gmail.com>
2024-03-19 16:13:32 +0100

    fix sass/ codice prezzo

--- resources/views/pages/comicsView/create.blade.php
+++ resources/views/pages/comicsView/create.blade.php
@@ -7,26 +7,41 @@
     <h1>Create new product</h1>
     <form action="{{route('comics.store')}}" method="POST">
         @csrf
-        <label for="ComicTitle">Comic Title</label>
-       <input type="text" name="title" id="ComicTitle">
+        <div>
+            <label for="ComicTitle">Comic Title</label>
+           <input type="text" name="title" id="ComicTitle">
 
-        <label for="description">Comic Desc.</label>
-       <input type="text" name="description" id="description">
+        </div>
+        <div>
+            <label for="description">Comic Desc.</label>
+           <input type="text" name="description" id="description">
 
-        <label for="thumb">Comic Image</label>
-       <input type="text" name="thumb" id="thumb" placeholder="e.g https://www.thisformat">
+        </div>
+<div>
+    <label for="thumb">Comic Image</label>
+   <input type="text" name="thumb" id="thumb" placeholder="e.g https://www.thisformat">
 
-        <label for="price">Comic Price</label>
-       <input type="number" name="price" id="price" min="1.00" placeholder="e.g 10.99" required>
+</div>
+<div>
+    <label for="price">Comic Price</label>
+   <input type="number" name="price" id="price" min="0.01" placeholder="e.g 10.99" step="0.01" required>
 
-       <label for="series">Comic Serie</label>
-       <input type="text" name="series" id="series" placeholder="e.g Batman">
+</div>
+<div>
+    <label for="series">Comic Serie</label>
+    <input type="text" name="series" id="series" placeholder="e.g Batman">
 
-        <label for="sale_date">Data Uscita: </label>
-       <input type="text" name="sale_date" placeholder="YYYY-MM-DD" pattern="\d{4}-\d{2}-\d{2}" title="sale_date" required>
+</div>
+<div>
+    <label for="sale_date">Data Uscita: </label>
+   <input type="text" name="sale_date" placeholder="YYYY-MM-DD" pattern="\d{4}-\d{2}-\d{2}" title="sale_date" required>
 
-       <label for="type">Comic Type</label>
-       <input type="text" name="type" id="type" placeholder="e.g Graphic Novel">
+</div>
+<div>
+    <label for="type">Comic Type</label>
+    <input type="text" name="type" id="type" placeholder="e.g Graphic Novel">
+
+</div>
 
         <button type="submit">Inserisci</button>
     </form>
